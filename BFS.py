9a2424823534faa906dfe785a3af4f972810f519
Nhựt Nguyễn <nguyenminhnhut3607@gmail.com>
2025-01-04 00:25:19 +0700

    Done BFS detect

--- BFS.py
+++ BFS.py
@@ -10,104 +10,99 @@ from IPython.display import Image
 
 from collections import deque
 
-def bfs(img,visited,i,j):
-    queue = deque([(i,j)])
-    visited[i][j] = True
-    pixel = []
-
+def bfs(img, pos, labels, count):
+    queue = deque([pos])
+    labels[pos] = count
+    total_pixel = 0
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     while queue:
         x, y = queue.popleft()
-        pixel.append((x,y))
-        for nx in range(x - 1, x + 2):  
-            for ny in range(y - 1, y + 2): 
-                if 0 <= nx < len(img) and 0 <= ny < len(img[0]):
-                    if not visited[nx][ny] and img[nx,ny] == 0:
-                        visited[nx][ny] = True
-                        queue.append((nx, ny)) 
-    return pixel                       
-
-def all_components(img):
-    rows, cols = img.shape
-    visited = [[False for _ in range(cols)] for _ in range(rows)]
-    components = 0
-
-    for i in range(rows):
-        for j in range(cols):
-            if not visited[i][j] and img[i,j] == 0:
-                bfs(img,visited,i,j)
-                components+=1
-
-    return components
-
-def objCollision(video_path):
-    cap = cv2.VideoCapture(video_path)
-    if not cap.isOpened():
-        print("Không thể mở video.")
-        exit()
-
-    while True:
-        ret, frame = cap.read()
-        current_time_ms = cap.get(cv2.CAP_PROP_POS_MSEC)
-
-        if not ret:
-            break
         
-        grad_magnitude = cv2.Canny(frame,100,200)
-        if all_components(frame) == 1:
-            print(f"Chạm nhau tại: {current_time_ms}")
-            break
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < len(img) and 0 <= ny < len(img[0]) and img[x,y] == img[nx,ny] and labels[nx,ny] == 0:
+                labels[nx,ny] = count
+                queue.append((nx, ny))
+                total_pixel +=1
+
+    return total_pixel
+
+def bfs_object_detection(binary_frame, min_objPixel = 10, max_distance=50):
+    rows, cols = binary_frame.shape
+    labels = np.zeros((rows,cols), dtype=int)
     
-    print("không chạm nhau")
-        
-        
-def bfs(img,visited,i,j):
-    queue = deque([(i,j)])
-    visited[i][j] = True
+    count = 1
+    object = []
 
-    while queue:
-        x, y = queue.popleft()
-        for nx in range(x - 1, x + 2):  
-            for ny in range(y - 1, y + 2): 
-                if 0 <= nx < len(img) and 0 <= ny < len(img[0]):
-                    if not visited[nx][ny] and img[x,y] == img[nx,ny]:
-                        visited[nx][ny] = True
-                        queue.append((nx, ny))                 
+    for x in range(rows):
+        for y in range(cols):
+            if binary_frame[x,y] > 0 and labels[x,y] == 0:
+                pixels = bfs(binary_frame, (x,y), labels, count)    
+                if pixels >= min_objPixel:
+                    xs, ys = np.where(labels == count)
+                    if len(xs) > 0 and len(ys) > 0:
+                        min_x, max_x = np.min(xs), np.max(xs)
+                        min_y, max_y = np.min(ys), np.max(ys)
+                        object.append((min_x, min_y, max_x, max_y,pixels))
+                count+=1
 
-def all_components(img):
-    rows, cols = img.shape
-    visited = [[False for _ in range(cols)] for _ in range(rows)]
-    components = 0
+    return object
 
-    for i in range(rows):
-        for j in range(cols):
-            if not visited[i][j]:
-                bfs(img,visited,i,j)
-                components+=1
+def tracking_colision(curr_area,pre_obj, tolerance_pixel=20):
+    for i in range(len(pre_obj)):
+        for j in range(i+1, len(pre_obj)):
+            pre_area1 = pre_obj[i][4]
+            pre_area2 = pre_obj[j][4]
+            total_preArea = pre_area1 + pre_area2
+            if total_preArea - tolerance_pixel <= curr_area <= total_preArea + tolerance_pixel:
+                return True
+    return False
 
-    return components
-
-def objCollision(video_path):
+def bfs_obj_collision(video_path,min_objPixel = 10, debug = False):
     cap = cv2.VideoCapture(video_path)
     if not cap.isOpened():
         print("Không thể mở video.")
         exit()
+    
+    if debug:
+        output_folder = 'frames_output'
+        os.makedirs(output_folder, exist_ok=True)
+
     frame_count = 0
-    pastComponents = 0
+    previous_objects = []
     while True:
         ret, frame = cap.read()
         current_time_ms = cap.get(cv2.CAP_PROP_POS_MSEC)
 
         if not ret:
-            print("không chạm nhau")
+            print("Hết video")
             break
 
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         blurred = cv2.GaussianBlur(gray_frame, (5, 5), 0)
-        _, binary = cv2.threshold(blurred, 147, 255, cv2.THRESH_BINARY_INV)
+        _, binary = cv2.threshold(blurred, 147, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
-        currentComponents = all_components(binary)
-        if pastComponents > currentComponents:
-            print(f"Chạm nhau tại: {current_time_ms}, Frame: {frame_count}")
-            break
-        pastComponents = currentComponents
+        #Tìm đối tượng
+        objects = bfs_object_detection(binary, min_objPixel)
+        
+        #có sự thay đổi thì tracking
+        if len(objects) < len(previous_objects):
+            for i in range(len(objects)):
+                curr_area = objects[i][4]
+                if tracking_colision(curr_area,previous_objects):
+                    print(f"Chạm nhau tại: {current_time_ms}, Frame: {frame_count}")
+        
+        previous_objects = objects
+            
+        if debug:
+            debug_frame = frame.copy()
+            for obj in objects:
+                min_x, min_y, max_x, max_y, area = obj
+                cv2.rectangle(debug_frame, (min_y, min_x), (max_y, max_x), (0, 255, 0), 2)
+
+            # Lưu khung hình có bounding boxes
+            frame_filename = os.path.join(output_folder, f'frame_{frame_count:04d}.png')
+            cv2.imwrite(frame_filename, debug_frame)
+            print(f"Frame {frame_count}: {len(objects)}")
+
         frame_count+=1
